4be33831d069a27acc94884e50691148a589d363
Jeonghoon-Lee <hoon22.lee@gmail.com>
2020-04-09 15:01:20 -0400

    first commit

---
+++ src/build/precache-manifest.91ae25f2395fcd6039ed629b86acaeb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6167bc116cee96c7658e1a7a087ed07c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24cca2e8667cca3281f9",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "eb21b5816088ede3a3f6",
+    "url": "/static/js/2.8d356d5a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8d356d5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24cca2e8667cca3281f9",
+    "url": "/static/js/main.158948f1.chunk.js"
+  },
+  {
+    "revision": "e988c064c70788dbc50e",
+    "url": "/static/js/runtime-main.a56884a5.js"
+  }
+]);
